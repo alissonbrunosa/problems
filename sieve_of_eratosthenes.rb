bff639eadb77e72310ea9968997e242f63ccc0fb
Alisson B. Santos <alissonbruno.sa@gmail.com>
2018-06-02 18:16:43 -0300

    Using recursion to solve sieve

--- sieve_of_eratosthenes.rb
+++ sieve_of_eratosthenes.rb
@@ -1,40 +1,13 @@
 class Eratosthenes
-  attr_reader :root, :max, :primes
-  ZERO = 0
-  FIRST_PRIME = 2
+  def self.call(max, numbers = nil)
+    numbers ||= (2..max).to_a
 
-  def initialize(max)
-    @max = max
-    @root = Math.sqrt(@max).floor
-    sieve
-  end
+    prime = numbers.first
 
-  def count
-    primes.count
-  end
+    return numbers if prime.nil? || prime * prime > max
 
-  def to_s
-    %|
-      Number of primes: #{count}
-      Primes: #{primes.join(" ")}
-    |
-  end
-
-  private
-
-  def sieve
-    numbers = (FIRST_PRIME..max).to_a
-    FIRST_PRIME.upto(root) do |number|
-      next unless numbers.include?(number)
-      numbers.reject! do |n|
-        next if n == number
-        n % number == ZERO
-      end
-    end
-    @primes ||= numbers
+    [prime].concat call(max, numbers.select { |n| n % prime != 0 })
   end
 end
 
-
-e = Eratosthenes.new(100)
-puts e
+p Eratosthenes.call(120)
